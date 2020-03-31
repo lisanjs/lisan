11b0935da8a2f70650994b99c5204860a6a952d7
yatki <mehmet@yatki.com>
2020-03-31 03:42:47 +0300

    Remove redundant bash script

--- copy.sh
+++
@@ -1,5 +0,0 @@
-# this file is temporary, needed during development, remove after publishing lisan-compiler
-
-cp ./packages/lisan-compiler/dist/index.umd.js ./website/static/js/lisan-compiler.min.js
-cp ./packages/lisan-compiler/dist/index.umd.js.map ./website/static/js/lisan-compiler.min.js.map
-
