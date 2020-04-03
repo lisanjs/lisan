249ce12265285bd9d918683c9fa32cc48feeff75
yatki <mehmet@yatki.com>
2020-04-03 16:30:19 +0300

    Remove defensive semicolon from lisan module

--- packages/lisan-compiler/src/lib/generate/templates/index.ts
+++ packages/lisan-compiler/src/lib/generate/templates/index.ts
@@ -5,7 +5,7 @@ const esm = (sourceCode: string): string => `export default ${sourceCode};\n`;
 const cjs = (sourceCode: string): string => `module.exports = ${sourceCode};\n`;
 
 const lisan = (sourceCode: string): string =>
-  `;(function(module){\n\nmodule.exports = ${sourceCode};\n\n})(typeof module === 'object' && module.exports ? module : window.lisanLoaderListener);\n`;
+  `(function(module){\n\nmodule.exports = ${sourceCode};\n\n})(typeof module === 'object' && module.exports ? module : window.lisanLoaderListener);\n`;
 
 const templates: Templates = { esm, cjs, lisan };
 
